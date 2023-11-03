425778f5bf63454b8be984cb6164a00f5bcbad56
yungae <chldbswp123@gmail.com>
2023-11-03 13:07:37 +0900

    commit2

---
+++ Flutting_Merge/script2.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const genderButtons = document.querySelectorAll(".gender-button");
+    const mbtiButtons = document.querySelector(".mbti-buttons");
+    const nextButton = document.getElementById("nextButton");
+
+    let selectedGender = null;
+
+    genderButtons.forEach(button => {
+      button.addEventListener("click", function() {
+        genderButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+        selectedGender = button.dataset.gender;
+      });
+    });
+
+    const mbtiOptions = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"];
+    mbtiOptions.forEach(option => {
+      const mbtiButton = document.createElement("button");
+      mbtiButton.classList.add("mbti-button");
+      mbtiButton.textContent = option;
+      mbtiButton.dataset.mbti = option;
+      mbtiButton.addEventListener("click", function() {
+        mbtiButtons.querySelectorAll(".mbti-button").forEach(btn => btn.classList.remove("active"));
+        mbtiButton.classList.add("active");
+      });
+      mbtiButtons.appendChild(mbtiButton);
+    });
+
+    
+    nextButton.addEventListener("click", function() {
+      const idealType = document.querySelector(".image-button.active")?.alt;
+    
+      if (!idealType) {
+        alert("MBTI 이상형을 선택해주세요.");
+        return;
+      }
+    
+      localStorage.setItem("idealType", idealType);
+      window.location.href = "test3.html";
+    });
+    
+
+});
+
